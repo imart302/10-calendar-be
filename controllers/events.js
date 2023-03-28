a04e48b88113c403abeed5707be28b2d3cf17b27
Iván Martínez <imartinezchin@gmail.com>
2023-03-28 12:48:49 -0600

    adding try catch blocks in controllers to handle errors

--- controllers/events.js
+++ controllers/events.js
@@ -3,145 +3,172 @@ const EventCalendar = require('../models/Event');
 const getEvents = async (req, res) => {
   const { start, end, page = 0, limit = 10 } = req.query;
 
-  let events = null;
-  const count = await EventCalendar.count({userId: req.uid});
-  if (start && end) {
-    events = await EventCalendar.find({
-      userId: req.uid,
-      $and: [
-        {
-          start: {
-            $gte: start,
+  try {
+    let events = null;
+    const count = await EventCalendar.count({ userId: req.uid });
+    if (start && end) {
+      events = await EventCalendar.find({
+        userId: req.uid,
+        $and: [
+          {
+            start: {
+              $gte: start,
+            },
           },
-        },
-        {
-          end: {
-            $lte: end,
+          {
+            end: {
+              $lte: end,
+            },
           },
-        },
-      ],
-    })
-      .skip(limit * page)
-      .limit(limit);
-  } else if (start) {
-    events = await EventCalendar.find({
-      userId: req.uid,
-      start: { $gte: start },
-    })
-      .skip(limit * page)
-      .limit(limit);
-  } else if (end) {
-    events = await EventCalendar.find({
-      userId: req.uid,
-      end: { $lte: end },
-    })
-      .skip(limit * page)
-      .limit(limit);
-  } else {
-    events = await EventCalendar.find({ userId: req.uid })
-      .skip(limit * page)
-      .limit(limit);
+        ],
+      })
+        .skip(limit * page)
+        .limit(limit);
+    } else if (start) {
+      events = await EventCalendar.find({
+        userId: req.uid,
+        start: { $gte: start },
+      })
+        .skip(limit * page)
+        .limit(limit);
+    } else if (end) {
+      events = await EventCalendar.find({
+        userId: req.uid,
+        end: { $lte: end },
+      })
+        .skip(limit * page)
+        .limit(limit);
+    } else {
+      events = await EventCalendar.find({ userId: req.uid })
+        .skip(limit * page)
+        .limit(limit);
+    }
+
+    const pagination = {
+      page,
+      offset: limit,
+      count,
+      remaining: count - limit * page - events.length,
+      data: events,
+    };
+
+    return res.json({
+      ok: true,
+      pagination,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
+    });
   }
-
-  const pagination = {
-    page,
-    offset: limit,
-    count,
-    remaining: count - limit * page - events.length,
-    data: events,
-  };
-
-  return res.json({
-    ok: true,
-    pagination,
-  });
 };
 
 const getEvent = async (req, res) => {
   const { id } = req.params;
 
-  const event = await EventCalendar.findById(id);
-  
-  if(!event){
-    return res.status(404).json({
-      message: 'Event not found',
+  try {
+    const event = await EventCalendar.findById(id);
+
+    if (!event) {
+      return res.status(404).json({
+        message: 'Event not found',
+      });
+    }
+
+    return res.json({
+      ok: true,
+      event,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
     });
   }
-
-  return res.json({
-    ok: true,
-    event,
-  });
-
-}
+};
 
 const createEvent = async (req, res) => {
-  const { title, notes="", start, end, bgColor = '#fafafa' } = req.body;
+  const { title, notes = '', start, end, bgColor = '#fafafa' } = req.body;
 
-  const event = new EventCalendar({
-    title,
-    notes,
-    start,
-    end,
-    bgColor,
-    userId: req.uid,
-  });
+  try {
+    const event = new EventCalendar({
+      title,
+      notes,
+      start,
+      end,
+      bgColor,
+      userId: req.uid,
+    });
 
-  event.save();
+    event.save();
 
-  return res.json({
-    ok: true,
-    event,
-  });
+    return res.json({
+      ok: true,
+      event,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
+    });
+  }
 };
 
 const deleteEvent = async (req, res) => {
   const { id } = req.params;
 
-  const event = await EventCalendar.findById(id);
+  try {
+    const event = await EventCalendar.findById(id);
 
-  if(!event){
-    return res.status(404).json({
-      message: 'Event not found'
+    if (!event) {
+      return res.status(404).json({
+        message: 'Event not found',
+      });
+    }
+
+    const result = await EventCalendar.deleteOne({
+      _id: id,
+    });
+    return res.json({
+      ok: true,
+      deleted: result.deletedCount,
+    });
+  } catch {
+    return res.status(500).json({
+      error,
     });
   }
-
-  const result = await EventCalendar.deleteOne({
-    _id: id,
-  });
-  return res.json({
-    ok: true,
-    deleted: result.deletedCount,
-  });
 };
 
 const updateEvent = async (req, res) => {
-
   const { id } = req.params;
-  
 
-  const event = await EventCalendar.findById(id);
+  try {
+    const event = await EventCalendar.findById(id);
 
-  if(!event){
-    return res.status(404).json({
-      message: 'Event not found',
+    if (!event) {
+      return res.status(404).json({
+        message: 'Event not found',
+      });
+    }
+
+    const { title, notes, start, end, bgColor } = req.body;
+
+    if (title) event.title = title;
+    if (notes) event.notes = notes;
+    if (start) event.start = start;
+    if (end) event.end = end;
+    if (bgColor) event.bgColor = bgColor;
+
+    const result = await event.save();
+
+    return res.json({
+      ok: true,
+      result,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
     });
   }
-
-  const { title, notes, start, end, bgColor} = req.body;
-
-  if(title) event.title = title;
-  if(notes) event.notes = notes;
-  if(start) event.start = start;
-  if(end) event.end = end;
-  if(bgColor) event.bgColor = bgColor;
-  
-  const result = await event.save();
-  
-  return res.json({
-    ok: true,
-    result,
-  });
 };
 
 module.exports = {
